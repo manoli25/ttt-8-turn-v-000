a1e1b02811c3336ba6c1042e5c250a00f8c3dc73
Manny Georganas <manoli.georganas@gmail.com>
2019-06-04 12:26:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,20 +1,20 @@
 
 
 def display_board(board)
-  return board
+  #return board
 end
 
 def valid_move?(board, index)
-  if XXX
-    return true
+  #if XXX
+  #  return true
 
 end
 
 def move(board, index)
-  puts ""
+  #puts ""
 end
 
 def input_to_index(input)
-  "convert user input to index"
-  return input
+  #"convert user input to index"
+  #return input
 end
